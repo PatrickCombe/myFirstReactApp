f84a11a1dc90cb1621d99cf50aa3ae7175ecc764
git status <patrick.combe@duke.edu>
2018-06-29 14:03:35 -0700

    2

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<p>Pat is making a React App</p>,
+var dummyData=['Go buy groceries', 'Get a haircut', 'Email John'];
+
+class TodoList extends React.Component{
+
+  render(){
+    return (
+      <ul>
+        {dummyData.map((task)=> <li><button>X</button> {task} </li>)}
+      </ul>
+    )
+
+  }
+
+
+
+}
+
+
+ReactDOM.render(<TodoList />,
    document.getElementById('root'));
